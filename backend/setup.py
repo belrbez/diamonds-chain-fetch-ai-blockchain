3ba6602ae6a31c684e4ffcc8728cc054d1b4ce2d
Dmitriy <demon007>
2019-06-19 01:37:08 +0300

    update setup.py

--- backend/setup.py
+++ backend/setup.py
@@ -1,8 +1,9 @@
 import setuptools
 
-requirements = ['flask', 'pickle', 'oef', 'fetchai', 'schedule']
+requirements = ['flask', 'pickle', 'oef', 'schedule']
 
 setuptools.setup(
     name='Backend',
+    version='0.0.1',
     packages=setuptools.find_packages(),
     install_requires=requirements)

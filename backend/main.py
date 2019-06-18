9ec321b2d4381a954cde15236aa9d8ff7a1bef66
Dmitriy <demon007>
2019-06-18 20:05:35 +0300

    add get method

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,8 @@ import asyncio
 
 app = flask.Flask(__name__)
 
+agents = {}
+
 
 def to_json(data):
     return json.dumps(data)
@@ -17,9 +19,16 @@ def add_journey_request():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     add_agent(data)
+    agents[data['id']] = data
     return 'ok'
+
+
+@app.route('/journey/<id>', methods=['GET'])
+def get_journey_request(id: str):
+    if id not in agents:
+        flask.abort(404)
+    return to_json(agents[id])
 
 
 if __name__ == '__main__':
     app.run(port=os.environ.get('SERVER_PORT', 8001))
-

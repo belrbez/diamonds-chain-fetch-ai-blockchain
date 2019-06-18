dd5761a9724bbd62dc389acde72ed740a622d91c
Dmitriy <demon007>
2019-06-18 19:28:08 +0300

    init backend

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+import flask
+import json
+import os
+from backend.agents.rider_agent import add_agent
+
+app = flask.Flask(__name__)
+
+
+def to_json(data):
+    return json.dumps(data)
+
+
+@app.route("/journey", methods=['POST'])
+def add_journey_request():
+    data = flask.request.json
+    add_agent(data)
+    return 'ok'
+
+
+if __name__ == '__main__':
+    app.run(port=os.environ.get('SERVER_PORT', 8001))
+

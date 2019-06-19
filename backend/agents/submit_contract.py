b978621ea2434e02cd45fecafbf5fc97398622a3
Alexander Vasiliev <alexandrvasilievby@gmail.com>
2019-06-19 10:12:01 +0300

    Smart Contract added.

--- backend/agents/submit_contract.py
+++ backend/agents/submit_contract.py
@@ -11,7 +11,7 @@ def call(name: str)-> SmartContract:
     address1 = Address(provider1)
 
     # Setting API up
-    api = LedgerApi('127.0.0.1', 8100)
+    api = LedgerApi('185.91.52.11', 1002)
 
     # Need funds to deploy contract
     api.sync(api.tokens.wealth(provider1, 59000000))
